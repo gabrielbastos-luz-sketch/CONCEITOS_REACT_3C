0707ecc4f29301ae4f2cc94caf3659169f4ce1d9
GABRIEL BASTOS LUZ <gabriel_bastos-luz@estudante.sesisenai.org.br>
2026-05-22 08:21:09 -0300

    boo

--- src/paginas/Login.jsx
+++ src/paginas/Login.jsx
@@ -6,7 +6,21 @@ export default function Login(){
 
     return(
         <div>
+            <input type="text" 
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            />
+            <input type="password" 
+            id="senha"
+            value={senha}
+            onChange={(e) => setSenha(e.target.value)}
+            placeholder="Senha"
+            />
 
+            <h3>{email}</h3>
+            <h3>{senha}</h3>
         </div>
     )
 }
