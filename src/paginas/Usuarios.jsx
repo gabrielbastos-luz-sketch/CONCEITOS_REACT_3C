9d06b6643b4b300315469938cffc1bb5014e3d0f
GABRIEL BASTOS LUZ <gabriel_bastos-luz@estudante.sesisenai.org.br>
2026-06-03 09:25:40 -0300

    edidel

--- src/paginas/Usuarios.jsx
+++ src/paginas/Usuarios.jsx
@@ -3,27 +3,68 @@ export default function Usuarios(){
     const [usuarios, setUsuarios] = useState([])
     const [cont, setCont] = useState(0)
     const [modal, setModal] = useState(false)
-
-    const aumenta = () => {
-        setCont(cont + 1)
-    }
+    const [nome, setNome] = useState('')
+    const [email, setEmail] = useState('')
+    const [senha, setSenha] = useState('')
+    const [id, setId] = useState('')
 
     useEffect(() => {
         document.title = `count: ${cont}`
-        const buscarUsuarios = async ()=>{
-            const resposta = await fetch('http://localhost:3000/usuarios')
-            const data = await resposta.json()
-            console.log(data)
-            setUsuarios(data)
-        }
         buscarUsuarios()
     }, [cont])
+
+    const buscarUsuarios = async ()=>{
+        const resultado = await fetch("http://localhost:3000/usuarios")
+        const data = await resultado.json()
+        console.log(data)
+        setUsuarios(data)
+    }
 
     const editar = (usuario) => {
         console.log('editando', usuario)
         setModal(true)
+        setEmail(usuario.email)
+        setSenha(usuario.senha)
+        setNome(usuario.nome)
+        setId(usuario.id)
     }
 
+    const confirmarEdicao = async () => {
+    const resultado = await fetch(
+        `http://localhost:3000/usuarios/${id}`,
+        {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email,
+                nome,
+                senha
+            })
+        }
+    )
+
+    const data = await resultado.json()
+
+    console.log(data)
+
+    setModal(false)
+
+    buscarUsuarios()
+}
+    const deletar = async (usuario) => {
+    const resultado = await fetch(
+        `http://localhost:3000/usuarios/${usuario.id}`,
+        {
+            method: 'DELETE'
+        }
+    )
+
+    console.log(resultado.status)
+
+    buscarUsuarios()
+}
     return (
         <div>
             <h1>Usuarios</h1>
@@ -36,8 +77,16 @@ export default function Usuarios(){
             <ul>
                 {usuarios.map((usuario) => (
                     <li key={usuario.id}>{usuario.nome}, 
-                    {usuario.email}
+                    {usuario.email},
+                    <br />
+
+                    STATUS: {usuario.ativo ? 'ativo' : 'inativo'}
+                    <br />
                     <button onClick={() => editar(usuario)}>Editar</button>
+                    <button onClick={() => deletar(usuario)}>Deletar</button>
+                    <br />
+
+                    
                     </li>
                 ))}
 
@@ -46,13 +95,32 @@ export default function Usuarios(){
             
 
             {modal && (
-                <div className="fundo-modal" onClick={()=> setModal(false)}>
+                <div className="fundo-modal">
                     <div className="modal-content">
                     <h1>Editar</h1>
+                    <input type="text"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="digite o Email"
+            />
+            <input type="text"
+                id="senha"
+                value={senha}
+                onChange={(e) => setSenha(e.target.value)}
+                placeholder="digite a Senha"
+            />
+            <input type="text" id="nome"
+            value={nome}
+            placeholder="digite seu nome"
+            onChange={(e)=>setSenha(e.target.value)} />
+            <button onClick={()=> setModal(false)}>fechar</button>
+            <button onClick={() => confirmarEdicao()}>confirmar edição</button>
+
                     </div>
                 </div>
             )}
-
+           
             {}
         </div>
     )
